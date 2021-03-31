22b5a5fb787ce828e1db477929bb066342b46ae8
Shyam Sunder Grover <post2shyam@gmail.com>
2021-03-31 18:46:04 +0530

    Added star and inr buttons to restaurant card.

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -8,8 +8,38 @@ class Home extends Component {
     this.state = {
       restaurants: [
         {
+          avg_rating: 4.0,
+          rating_count: 200,
+          pricing: 750,
           item_types: ["Indian", "Continental"],
           restaurant_name: "3 Wise Monkeys",
+          restaurant_image_url:
+            "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg",
+        },
+        {
+          avg_rating: 4.5,
+          rating_count: 2008,
+          pricing: 500,
+          item_types: ["Chinese", "Mughlai"],
+          restaurant_name: "Kung-fu Murgh",
+          restaurant_image_url:
+            "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg",
+        },
+        {
+          avg_rating: 4.1,
+          rating_count: 200,
+          pricing: 300,
+          item_types: ["South Indian", "North Indian"],
+          restaurant_name: "Kashmir se KanyaKumari",
+          restaurant_image_url:
+            "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg",
+        },
+        {
+          avg_rating: 3.5,
+          rating_count: 350,
+          pricing: 2300,
+          item_types: ["French", "German"],
+          restaurant_name: "The Grapes Valley",
           restaurant_image_url:
             "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg",
         },
