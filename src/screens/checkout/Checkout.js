eb79503a37ba699e2cb480b98cb9c28981b31445
simpleprahalad <simpleprahalad@gmail.com>
2021-03-31 08:24:08 +0530

    added dummy address and displaying the same in the "existing address" tab.

--- src/screens/checkout/Checkout.js
+++ src/screens/checkout/Checkout.js
@@ -10,6 +10,8 @@ import Typography from "@material-ui/core/Typography";
 import AppBar from "@material-ui/core/AppBar";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
+import PropTypes from "prop-types";
+import Card from "@material-ui/core/Card";
 
 const styles = (theme) => ({
   root: {
@@ -31,12 +33,55 @@ function getSteps() {
   return ["Delivery", "Payment"];
 }
 
+function TabPanel(props) {
+  return (
+    <div
+      role="tabpanel"
+      hidden={props.value !== props.index}
+      id={`simple-tabpanel-${props.index}`}
+      aria-labelledby={`simple-tab-${props.index}`}
+    >
+      {props.value === props.index && <Typography>{props.children}</Typography>}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
 class Checkout extends Component {
   constructor() {
     super();
     this.state = {
       activeStep: 0,
       value: 0,
+      existingAddresses: [
+        {
+          id: 100,
+          flat_buil_number: "B113",
+          flat_building_name: "SVS Palsm1",
+          locality: "Marathalli",
+          city: "Bangalore",
+          pincode: 560037,
+          state: {
+            state_name: "Karnataka",
+          },
+        },
+        {
+          id: 101,
+          flat_buil_number: "B112",
+          flat_building_name: "SVS Palsm1",
+          locality: "Marathalli",
+          city: "Bangalore",
+          pincode: 560037,
+          state: {
+            state_name: "Karnataka",
+          },
+        },
+      ],
     };
   }
 
@@ -56,16 +101,52 @@ class Checkout extends Component {
     switch (step) {
       case 0:
         return (
-          <AppBar position="static">
-            <Tabs
-              value={this.state.value}
-              onChange={this.tabChangeHandler}
-              aria-label="simple tabs example"
-            >
-              <Tab label="Existing Address" />
-              <Tab label="New Address" />
-            </Tabs>
-          </AppBar>
+          <div>
+            <AppBar position="static">
+              <Tabs
+                value={this.state.value}
+                onChange={this.tabChangeHandler}
+                aria-label="simple tabs example"
+              >
+                <Tab label="Existing Address" />
+                <Tab label="New Address" />
+              </Tabs>
+            </AppBar>
+            <TabPanel value={this.state.value} index={0}>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+              >
+                {this.state.existingAddresses.length > 0 ? (
+                  this.state.existingAddresses.map((value) => (
+                    <Card className={this.props.card} key={value.id}>
+                      <Typography>
+                        {value.flat_buil_number}
+                        <br />
+                        {value.flat_building_name}
+                        <br />
+                        {value.locality}
+                        <br />
+                        {value.city}
+                        <br />
+                        {value.state.state_name}
+                        <br />
+                        {value.pincode}
+                      </Typography>
+                    </Card>
+                  ))
+                ) : (
+                  <p style={{ color: "gray", height: 200 }}>
+                    There are no saved addresses! You can save an address using
+                    the 'New Address' tab or using your ‘Profile’ menu option.
+                  </p>
+                )}
+              </div>
+            </TabPanel>
+          </div>
         );
       case 1:
         return "An ad group contains one or more ads which target a shared set of keywords.";
